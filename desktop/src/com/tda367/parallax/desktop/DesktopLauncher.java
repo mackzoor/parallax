68eb861f23a2ee9bda5f6ac597c36c5f5068f972
mackzoor <markus.pettersson97@gmail.com>
2017-04-03 15:33:38 +0200

    Initial commit

---
+++ desktop/src/com/tda367/parallax/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tda367.parallax.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tda367.parallax.Parallax;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Parallax(), config);
+	}
+}
