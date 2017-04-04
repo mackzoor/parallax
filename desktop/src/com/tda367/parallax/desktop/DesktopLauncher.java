66e024df9c789eec1d48111c7e3aaba0728ffd37
Anthony <amk1997@hotmail.se>
2017-04-04 17:22:53 +0200

    Changed launchers to run the spaceCraft display instead of the default application.

--- desktop/src/com/tda367/parallax/desktop/DesktopLauncher.java
+++ desktop/src/com/tda367/parallax/desktop/DesktopLauncher.java
@@ -3,10 +3,11 @@ package com.tda367.parallax.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.tda367.parallax.Parallax;
+import com.tda367.parallax.SpaceCraftDisplay;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Parallax(), config);
+		new LwjglApplication(new SpaceCraftDisplay(), config);
 	}
 }
