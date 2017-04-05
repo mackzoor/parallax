91c4a39a1a4c0330ca7d6d69e3e7cb297ae54629
mackzoor <markus.pettersson97@gmail.com>
2017-04-05 14:02:54 +0200

    Added tests in separate test directory

--- core/test/com/tda367/parallax/test/AgelionTest.java
+++ core/test/com/tda367/parallax/test/AgelionTest.java
@@ -1,64 +1,73 @@
 package com.tda367.parallax.test;
 
-import com.tda367.parallax.parallaxCore.Agelion;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 /**
- * Created by Anthony on 05/04/2017.
+ * Created by Markus on 2017-04-05.
  */
 public class AgelionTest {
-
-    Agelion agelion;
-
     @Before
     public void setUp() throws Exception {
-    agelion = new Agelion();
+
     }
 
     @After
     public void tearDown() throws Exception {
+
     }
 
     @Test
     public void setSpeedTarget() throws Exception {
+
     }
 
     @Test
     public void setAccelerateTarget() throws Exception {
+
     }
 
     @Test
     public void setPanXTarget() throws Exception {
+
     }
 
     @Test
     public void setPanZTarget() throws Exception {
+
     }
 
     @Test
     public void setPanXAcceleration() throws Exception {
+
     }
 
     @Test
     public void setPanYAcceleration() throws Exception {
+
     }
 
     @Test
     public void action() throws Exception {
+
     }
 
     @Test
     public void getPos() throws Exception {
+
     }
 
     @Test
     public void getRot() throws Exception {
+
     }
 
     @Test
     public void update() throws Exception {
+
     }
 
 }
---
+++ core/test/com/tda367/parallax/test/Matrix3DTest.java
@@ -0,0 +1,10 @@
+package com.tda367.parallax.test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Markus on 2017-04-05.
+ */
+public class Matrix3DTest {
+
+}
