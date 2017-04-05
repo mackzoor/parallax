31b7492a604e96540201f92f6d4c66edd34ef86b
Anthony <amk1997@hotmail.se>
2017-04-05 09:13:54 +0200

    Added interface for collidable and updatable objects.

---
+++ GalactiCore/src/Collidable.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Anthony on 03/04/2017.
+ */
+public interface Collidable {
+
+    Vector3D getPos();
+    Matrix3D getRot();
+
+    //Get geo?
+
+}
---
+++ GalactiCore/src/Updatable.java
@@ -0,0 +1,6 @@
+/**
+ * Interface for objects that need to be updated every game tick/loop.
+ */
+public interface Updatable {
+    void update();
+}
