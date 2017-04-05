861b7cd0d97b086b5aab720779ddd327457ce715
Anthony <amk1997@hotmail.se>
2017-04-05 15:34:26 +0200

    Added camera class.

---
+++ core/src/com/tda367/parallax/parallaxCore/Camera.java
@@ -0,0 +1,49 @@
+package com.tda367.parallax.parallaxCore;
+
+/**
+ * Created by Anthony on 05/04/2017.
+ */
+public class Camera {
+
+    private Vector3D pos;
+    private Matrix3D rot;
+
+    private float fov;
+
+    private Collidable trackingTarget;
+
+    //TODO implement tracking mode, state pattern?
+    /*
+    public enum TrackingMode {
+
+        FOLLOW_AND_PAN,FOLLOW_AND_TRACK
+
+    }*/
+
+
+    public Camera(Vector3D pos, Matrix3D rot, float fov){
+        this.pos = pos;
+        this.rot = rot;
+        this.fov = fov;
+    }
+    public Camera(){
+        new Camera(new Vector3D(), new Matrix3D(), 90);
+    }
+
+    public void trackTo(Collidable collidable){
+        trackingTarget = collidable;
+    }
+    public void trackMode(){
+
+    }
+
+    public Vector3D getPos() {
+        return pos;
+    }
+    public Matrix3D getRot() {
+        return rot;
+    }
+    public float getFov() {
+        return fov;
+    }
+}
