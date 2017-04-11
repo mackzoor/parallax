b66db0ad0ffc6f50d1c9d62af6b64392713c708a
Lingondricka <ras.lindgren@telia.com>
2017-04-11 09:06:29 +0200

    Created classes for the powerUps

---
+++ core/src/com/tda367/parallax/parallaxCore/powerUps/Missile.java
@@ -0,0 +1,20 @@
+package com.tda367.parallax.parallaxCore.powerUps;
+
+import javax.vecmath.Quat4f;
+import javax.vecmath.Vector3f;
+
+/**
+ * Created by Rasmus on 2017-04-11.
+ */
+public class Missile implements IPowerUp {
+
+    @Override
+    public void activate() {
+
+    }
+
+    @Override
+    public void usePU(Vector3f pos, Quat4f rot) {
+
+    }
+}
---
+++ core/src/com/tda367/parallax/parallaxCore/powerUps/Shield.java
@@ -0,0 +1,14 @@
+package com.tda367.parallax.parallaxCore.powerUps;
+
+import com.tda367.parallax.parallaxCore.Usable;
+
+/**
+ * Created by Rasmus on 2017-04-11.
+ */
+public class Shield implements Usable {
+
+    @Override
+    public void activate() {
+
+    }
+}
