1ed11d0596d7984c908df7f3273e5d59e3f55b13
mackzoor <markus.pettersson97@gmail.com>
2017-04-11 09:30:39 +0200

    Added Cardboard demo

--- android/src/com/tda367/parallax/AndroidLauncher.java
+++ android/src/com/tda367/parallax/AndroidLauncher.java
@@ -2,15 +2,16 @@ package com.tda367.parallax;
 
 import android.os.Bundle;
 
-import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.tda367.parallax.platform.ParallaxLibGdxLayer;
+import com.badlogic.gdx.backends.android.CardBoardAndroidApplication;
+import com.tda367.parallax.platform.ParallaxCardboardLayer;
 
-public class AndroidLauncher extends AndroidApplication {
+public class AndroidLauncher extends CardBoardAndroidApplication {
+
 	@Override
-	protected void onCreate (Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new ParallaxLibGdxLayer(), config);
+		initialize(new ParallaxCardboardLayer(), config);
 	}
 }
---
+++ core/src/com/tda367/parallax/platform/ParallaxCardboardLayer.java
@@ -0,0 +1,114 @@
+package com.tda367.parallax.platform;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.android.CardBoardApplicationListener;
+import com.badlogic.gdx.backends.android.CardboardCamera;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Matrix4;
+import com.google.vrtoolkit.cardboard.Eye;
+import com.google.vrtoolkit.cardboard.HeadTransform;
+import com.google.vrtoolkit.cardboard.Viewport;
+
+/**
+ * Created by Markus on 2017-04-11.
+ */
+public class ParallaxCardboardLayer implements CardBoardApplicationListener {
+
+    private CardboardCamera cam;
+    private Model model;
+    private ModelInstance instance;
+    private ModelBatch batch;
+    private Environment environment;
+    private static final float Z_NEAR = 0.1f;
+    private static final float Z_FAR = 1000.0f;
+    private static final float CAMERA_Z = 0.01f;
+
+    @Override
+    public void create() {
+        cam = new CardboardCamera();
+        cam.position.set(0f, 0f, CAMERA_Z);
+        cam.lookAt(0,0,0);
+        cam.near = Z_NEAR;
+        cam.far = Z_FAR;
+
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+        ModelBuilder modelBuilder = new ModelBuilder();
+        model = modelBuilder.createBox(10f, 20f, 30f,
+                new Material(ColorAttribute.createDiffuse(Color.RED)),
+                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+        instance = new ModelInstance(model);
+        instance.transform.translate(0, 0, -50);
+
+        batch = new ModelBatch();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void render() {
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        model.dispose();
+    }
+
+    @Override
+    public void onNewFrame(HeadTransform paramHeadTransform) {
+        instance.transform.rotate(0, 1, 0, Gdx.graphics.getDeltaTime() * 30);
+    }
+
+    @Override
+    public void onDrawEye(Eye eye) {
+        Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        // Apply the eye transformation to the camera.
+        cam.setEyeViewAdjustMatrix(new Matrix4(eye.getEyeView()));
+
+        float[] perspective = eye.getPerspective(Z_NEAR, Z_FAR);
+        cam.setEyeProjection(new Matrix4(perspective));
+        cam.update();
+
+        batch.begin(cam);
+        batch.render(instance, environment);
+        batch.end();
+    }
+
+    @Override
+    public void onFinishFrame(Viewport paramViewport) {
+
+    }
+
+    @Override
+    public void onRendererShutdown() {
+
+    }
+
+    @Override
+    public void onCardboardTrigger() {
+
+    }
+}
