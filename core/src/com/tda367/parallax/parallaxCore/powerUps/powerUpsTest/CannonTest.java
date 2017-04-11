20a0f97a615208780d1b6c32e8199fb407ff08c7
Lingondricka <ras.lindgren@telia.com>
2017-04-11 10:06:19 +0200

    Created tests for all powerUp classes

---
+++ core/src/com/tda367/parallax/parallaxCore/powerUps/powerUpsTest/CannonTest.java
@@ -0,0 +1,19 @@
+package com.tda367.parallax.parallaxCore.powerUps.powerUpsTest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Rasmus on 2017-04-11.
+ */
+public class CannonTest {
+    @Test
+    public void activate() throws Exception {
+    }
+
+    @Test
+    public void usePU() throws Exception {
+    }
+
+}
