8fd489271c2c3b661fbb5b325c66485516f3bd97
NinjaWazzzabi <amk1997@hotmail.se>
2017-04-12 13:46:19 +0200

    Added support for xbox 360 controller.

--- core/src/com/tda367/parallax/platform/ParallaxLibGDXController.java
+++ core/src/com/tda367/parallax/platform/ParallaxLibGDXController.java
@@ -4,6 +4,11 @@ import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.controllers.Controller;
+import com.badlogic.gdx.controllers.ControllerListener;
+import com.badlogic.gdx.controllers.Controllers;
+import com.badlogic.gdx.controllers.PovDirection;
+import com.badlogic.gdx.math.Vector3;
 import com.tda367.parallax.parallaxCore.Player;
 
 import javax.vecmath.Vector2f;
@@ -11,13 +16,22 @@ import javax.vecmath.Vector2f;
 /**
  * Created by Markus on 2017-04-11.
  */
-public class ParallaxLibGDXController implements InputProcessor, IScreenControllerListener {
+public class ParallaxLibGDXController implements InputProcessor, IScreenControllerListener, ControllerListener {
 
-    Player player;
+    private Controller controller;
+    private Player player;
     OnScreenController onScreenController;
 
 
     ParallaxLibGDXController(Player player) {
+        Controllers.addListener(this);
+        for (Controller controller : Controllers.getControllers()) {
+            System.out.println(controller.getName());
+            this.controller = controller;
+            controller.addListener(this);
+        }
+
+
         Gdx.input.setInputProcessor(this);
         this.player = player;
         if (Gdx.app.getType() == Application.ApplicationType.Android) {
@@ -98,8 +112,77 @@ public class ParallaxLibGDXController implements InputProcessor, IScreenControll
             System.out.println("right");
             player.getSpaceCraft().addPanVelocity(new Vector2f(panSpeed, 0));
         }
+    }
+
+    @Override
+    public void connected(Controller controller) {
 
     }
+
+    @Override
+    public void disconnected(Controller controller) {
+
+    }
+
+    @Override
+    public boolean buttonDown(Controller controller, int buttonCode) {
+        return false;
+    }
+
+    @Override
+    public boolean buttonUp(Controller controller, int buttonCode) {
+        return false;
+    }
+
+    @Override
+    public boolean axisMoved(Controller controller, int axisCode, float value) {
+
+        // Xbox 360 controller axis
+        // 0 == Left yAxis +DOWN
+        // 1 == Left xAxis +RIGHT
+        // 2 == Right yAxis +DOWN
+        // 3 == Right xAxis +RIGHT
+
+        System.out.println(axisCode);
+
+        float panSpeed = 10;
+
+        float xMove = panSpeed*controller.getAxis(1);
+        float yMove = panSpeed*controller.getAxis(0)*-1;
+
+        player.getSpaceCraft().setPanVelocity(
+                new Vector2f(
+                        xMove,
+                        yMove
+                )
+        );
+
+
+        return false;
+    }
+
+    @Override
+    public boolean povMoved(Controller controller, int povCode, PovDirection value) {
+        return false;
+    }
+
+    @Override
+    public boolean xSliderMoved(Controller controller, int sliderCode, boolean value) {
+        return false;
+    }
+
+    @Override
+    public boolean ySliderMoved(Controller controller, int sliderCode, boolean value) {
+        return false;
+    }
+
+    @Override
+    public boolean accelerometerMoved(Controller controller, int accelerometerCode, Vector3 value) {
+        return false;
+    }
+
+
+
 
     @Override
     public void onUpdate() {
