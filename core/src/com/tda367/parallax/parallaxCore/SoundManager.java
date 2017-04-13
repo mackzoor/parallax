3ff349dc6fb56f20fe30d42ef64e235b29ae4b9f
Ludvig "Support" Andersson <Ludvand@student.chalmers.se>
2017-04-13 10:25:44 +0200

    Added a nestled class in soundManager to avoid using javaFX.

--- core/src/com/tda367/parallax/parallaxCore/SoundManager.java
+++ core/src/com/tda367/parallax/parallaxCore/SoundManager.java
@@ -1,21 +1,33 @@
 package com.tda367.parallax.parallaxCore;
-
-import com.badlogic.gdx.audio.Music;
-import com.tda367.parallax.platform.Sound;
-import javafx.util.Pair;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by xoxLU on 2017-04-12.
  */
 public class SoundManager {
 
+    private final class SoundCombiantion {
+        private final String fileName;
+        private final Float volume;
+
+        private SoundCombiantion(String fileName, Float volume) {
+            this.fileName = fileName;
+            this.volume = volume;
+        }
+
+        public String getFileName() {
+            return fileName;
+        }
+
+        public Float getVolume() {
+            return volume;
+        }
+    }
+
     private List<SoundListener> listeners = new ArrayList<SoundListener>();
-    private List<Pair<String,Float>> soundQueue = new ArrayList<Pair<String, Float>>();
-    private List<Pair<String,Float>> musicQueue = new ArrayList<Pair<String, Float>>();
+    private List<SoundCombiantion> soundQueue = new ArrayList<SoundCombiantion>();
+    private List<SoundCombiantion> musicQueue = new ArrayList<SoundCombiantion>();
 
     private static SoundManager instance;
     public static SoundManager getInstance(){
@@ -30,24 +42,24 @@ public class SoundManager {
 
     private void playQueuedSoundAndMusic(){
         for (int i = 0; i < soundQueue.size(); i++){
-            if(soundQueue.get(i).getValue() == 1f){
-                playMusic(soundQueue.get(i).getKey());
+            if(soundQueue.get(i).getVolume() == 1f){
+                playMusic(soundQueue.get(i).getFileName());
             } else{
-                playMusic(soundQueue.get(i).getKey(), soundQueue.get(i).getValue());
+                playMusic(soundQueue.get(i).getFileName(), soundQueue.get(i).getVolume());
             }
         }
         for (int i = 0; i < musicQueue.size(); i++){
-            if(musicQueue.get(i).getValue() == 1f){
-                playMusic(musicQueue.get(i).getKey());
+            if(musicQueue.get(i).getVolume() == 1f){
+                playMusic(musicQueue.get(i).getFileName());
             } else{
-                playMusic(musicQueue.get(i).getKey(), musicQueue.get(i).getValue());
+                playMusic(musicQueue.get(i).getFileName(), musicQueue.get(i).getVolume());
             }
         }
     }
 
     public void playSound(String sound){
         if(listeners.size() < 1){
-            soundQueue.add(new Pair<String, Float>(sound, 1f));
+            soundQueue.add(new SoundCombiantion(sound, new Float(1f)));
         }
         // Notify listeners to play sound.
         for (SoundListener Sl : listeners){
@@ -55,9 +67,9 @@ public class SoundManager {
         }
     }
 
-    public void playSound(String sound, float volume){
+    public void playSound(String sound, Float volume){
         if(listeners.size() < 1){
-            soundQueue.add(new Pair<String, Float>(sound, volume));
+            soundQueue.add(new SoundCombiantion(sound, volume));
         }
         // Notify listeners to play sound.
         for (SoundListener Sl : listeners){
@@ -67,7 +79,7 @@ public class SoundManager {
 
     public void playMusic(String music){
         if(listeners.size() < 1){
-            musicQueue.add(new Pair<String, Float>(music, 1f));
+            musicQueue.add(new SoundCombiantion(music, new Float(1f)));
         }
         // Notify listeners to play music.
         for (SoundListener Sl : listeners){
@@ -75,9 +87,9 @@ public class SoundManager {
         }
     }
 
-    public void playMusic(String music, float volume){
+    public void playMusic(String music, Float volume){
         if(listeners.size() < 1){
-            musicQueue.add(new Pair(music,volume));
+            musicQueue.add(new SoundCombiantion(music,volume));
         }
         // Notify listeners to play music.
         for (SoundListener Sl : listeners){
