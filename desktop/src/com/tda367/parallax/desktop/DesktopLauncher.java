2cbf31312dd0db46d930658e0e7adf4d64ead4ec
Ludvig "Support" Andersson <Ludvand@student.chalmers.se>
2017-04-26 09:48:42 +0200

    Changed the cannonLow sound file, changing the ending.

--- desktop/src/com/tda367/parallax/desktop/DesktopLauncher.java
+++ desktop/src/com/tda367/parallax/desktop/DesktopLauncher.java
@@ -3,7 +3,6 @@ package com.tda367.parallax.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.tda367.parallax.platform.ParallaxLibGdxLayer;
-import com.tda367.parallax.platform.SpaceCraftDisplay;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
