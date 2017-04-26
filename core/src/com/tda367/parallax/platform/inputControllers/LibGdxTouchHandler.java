0ea9ea04ec1d3ad4c2b734b2fe98a9ec4ca6c78e
mackzoor <markus.pettersson97@gmail.com>
2017-04-26 15:17:59 +0200

    Removed OnScreenTouchpad to eliminate "middle man"

--- core/src/com/tda367/parallax/platform/inputControllers/LibGdxTouchHandler.java
+++ core/src/com/tda367/parallax/platform/inputControllers/LibGdxTouchHandler.java
@@ -1,20 +1,54 @@
 package com.tda367.parallax.platform.inputControllers;
 
-import com.tda367.parallax.platform.inputControllers.onScreenTouchpad.IScreenControllerListener;
-import com.tda367.parallax.platform.inputControllers.onScreenTouchpad.OnScreenTouchpad;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
 /**
  * Created by Markus on 2017-04-25.
  */
 
-public class LibGdxTouchHandler implements IScreenControllerListener {
+public class LibGdxTouchHandler implements EventListener {
+
+    private Touchpad.TouchpadStyle touchpadStyle;
+    private Touchpad touchpad;
+    private Drawable touchBackground;
+    private Drawable touchKnob;
+    private Skin touchpadSkin;
+    private Stage stage;
+
 
     private InputControlsListener listener;
-    private OnScreenTouchpad onScreenTouchpad;
 
     public LibGdxTouchHandler() {
-        onScreenTouchpad = new OnScreenTouchpad();
-        onScreenTouchpad.setListener(this);
+        touchpadSkin = new Skin();
+        touchpadSkin.add("background", new Texture(
+                "touchpad/background.png"));
+        touchpadSkin.add("knob", new Texture("touchpad/knob.png"));
+
+        touchpadStyle = new Touchpad.TouchpadStyle();
+
+        touchBackground = touchpadSkin.getDrawable("background");
+
+        touchKnob = touchpadSkin.getDrawable("knob");
+        touchKnob.setMinWidth(200);
+        touchKnob.setMinHeight(200);
+
+        touchpadStyle.background = touchBackground;
+        touchpadStyle.knob = touchKnob;
+        touchpad = new Touchpad(10, touchpadStyle);
+
+        touchpad.setBounds(45, 45, 400, 400);
+        touchpad.addListener(this);
+
+        stage = new Stage();
+        stage.addActor(touchpad);
+        Gdx.input.setInputProcessor(stage);
     }
 
     public void setListener(InputControlsListener listener) {
@@ -22,12 +56,23 @@ public class LibGdxTouchHandler implements IScreenControllerListener {
     }
 
     public void drawTouchPad(){
-        onScreenTouchpad.drawTouchPad();
+        stage.draw();
     }
 
-    @Override
+    public void alertListener(){
+        if(listener != null){
+            onUpdate(touchpad.getKnobPercentX(),touchpad.getKnobPercentY());
+        }
+    }
+
     public void onUpdate(float x, float y) {
         listener.xAxisJoystickMovement(x);
         listener.yAxisJoystickMovement(y);
+    }
+
+    @Override
+    public boolean handle(Event event) {
+        alertListener();
+        return false;
     }
 }
