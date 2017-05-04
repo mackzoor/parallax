665f00c56beb7b53310fab0a2eddbf976ec89ddc
mackzoor <markus.pettersson97@gmail.com>
2017-05-04 20:25:29 +0200

    Begin implementation of Game and Screens

---
+++ core/src/com/tda367/parallax/controller/gamestates/MenuScreen.java
@@ -0,0 +1,52 @@
+package com.tda367.parallax.controller.gamestates;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by Markus on 2017-05-04.
+ */
+
+public class MenuScreen implements Screen {
+
+    Game game;
+
+    MenuScreen(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
