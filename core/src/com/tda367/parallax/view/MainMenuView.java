51adb866673f1a713ebc3b666debfaa4c585817f
mackzoor <markus.pettersson97@gmail.com>
2017-05-08 10:46:57 +0200

    Implemented view part

--- core/src/com/tda367/parallax/view/MainMenuView.java
+++ core/src/com/tda367/parallax/view/MainMenuView.java
@@ -1,5 +1,9 @@
 package com.tda367.parallax.view;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.tda367.parallax.model.menu.mainmenu.MainMenuModel;
 
 /**
@@ -10,11 +14,65 @@ public class MainMenuView {
 
     private MainMenuModel mainMenuModel;
 
+    private SpriteBatch batch;
+    private Texture background;
+    private Texture unSelectedPlayButton;
+    private Texture selectedPlayButton;
+    private Texture unSelectedExitButton;
+    private Texture selectedExitButton;
+
     public MainMenuView(MainMenuModel mainMenuModel) {
+
         this.mainMenuModel = mainMenuModel;
+
+        batch = new SpriteBatch();
+        background = new Texture("background");
+        unSelectedPlayButton = new Texture("playWhite.png");
+        selectedPlayButton = new Texture("playButtonFocus.png");
+        unSelectedExitButton = new Texture("exitWhite.png");
+        selectedExitButton = new Texture("exitButtonFocus.png");
     }
 
     public void render() {
-        //Render all the things!
+
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+
+        if (mainMenuModel.getStartButton().isMarked()) {
+            batch.draw(
+                    selectedPlayButton,
+                    mainMenuModel.getStartButton().getPos().getX(),
+                    mainMenuModel.getStartButton().getPos().getY(),
+                    mainMenuModel.getStartButton().getWidth(),
+                    mainMenuModel.getStartButton().getHeight()
+            );
+        } else {
+            batch.draw(
+                    unSelectedPlayButton,
+                    mainMenuModel.getStartButton().getPos().getX(),
+                    mainMenuModel.getStartButton().getPos().getY(),
+                    mainMenuModel.getStartButton().getWidth(),
+                    mainMenuModel.getStartButton().getHeight()
+            );
+        }
+
+        if (mainMenuModel.getExitButton().isMarked()) {
+            batch.draw(
+                    selectedExitButton,
+                    mainMenuModel.getExitButton().getPos().getX(),
+                    mainMenuModel.getExitButton().getPos().getY(),
+                    mainMenuModel.getExitButton().getWidth(),
+                    mainMenuModel.getExitButton().getHeight()
+            );
+        } else {
+            batch.draw(
+                    unSelectedExitButton,
+                    mainMenuModel.getExitButton().getPos().getX(),
+                    mainMenuModel.getExitButton().getPos().getY(),
+                    mainMenuModel.getExitButton().getWidth(),
+                    mainMenuModel.getExitButton().getHeight()
+            );
+        }
     }
 }
