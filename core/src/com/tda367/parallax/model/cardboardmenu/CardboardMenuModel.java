f7b1cd0443f0ce33ba5926fe31bae884f068efe0
Lingondricka <ras.lindgren@telia.com>
2017-05-10 14:53:59 +0200

    Made basic cardboard "menu world"

---
+++ core/src/com/tda367/parallax/model/cardboardmenu/CardboardMenuModel.java
@@ -0,0 +1,51 @@
+package com.tda367.parallax.model.cardboardmenu;
+
+import com.tda367.parallax.model.coreabstraction.Model;
+import com.tda367.parallax.model.coreabstraction.RenderManager;
+import com.tda367.parallax.model.coreabstraction.Renderable;
+
+import javax.vecmath.Quat4f;
+import javax.vecmath.Vector3f;
+
+/**
+ * Created by Rasmus on 2017-05-08.
+ */
+public class CardboardMenuModel implements Renderable {
+
+    private Vector3f pos;
+    private Quat4f rot;
+
+    private Model model;
+
+    public CardboardMenuModel(){
+        model = new Model("course.g3db", "3dModels/defaultCourse");
+        pos = new Vector3f();
+        rot = new Quat4f();
+
+    }
+
+    @Override
+    public Model getModel() {
+        return model;
+    }
+
+    @Override
+    public void addToRenderManager() {
+        RenderManager.getInstance().addRenderTask(this);
+    }
+
+    @Override
+    public void removeFromRenderManager() {
+        RenderManager.getInstance().removeRenderTask(this);
+    }
+
+    @Override
+    public Vector3f getPos() {
+        return pos;
+    }
+
+    @Override
+    public Quat4f getRot() {
+        return rot;
+    }
+}
---
+++ core/src/com/tda367/parallax/model/cardboardmenu/MainMenu.java
@@ -0,0 +1,51 @@
+package com.tda367.parallax.model.cardboardmenu;
+
+import com.tda367.parallax.model.coreabstraction.RenderManager;
+import com.tda367.parallax.model.coreabstraction.Updatable;
+import com.tda367.parallax.model.parallaxcore.Camera;
+import com.tda367.parallax.model.parallaxcore.Parallax;
+import com.tda367.parallax.model.parallaxcore.Player;
+
+
+/**
+ * Created by Rasmus on 2017-05-08.
+ */
+public class MainMenu implements Updatable {
+
+
+    Player player;
+    Parallax parallax;
+    CardboardMenu cbMenu;
+    Camera camera;
+    RenderManager renderManager;
+
+    public MainMenu(Player player){
+        this.player = player;
+        cbMenu = new CardboardMenu();
+        this.camera = new Camera();
+
+    }
+
+    public void startGame(){
+        parallax = new Parallax(player);
+    }
+
+    public void exitGame(){
+        //Ends the game
+    }
+
+    public void settingsPressed(){
+        //enters settings menu
+    }
+
+
+    @Override
+    public void update(int milliSinceLastUpdate) {
+        camera.update(milliSinceLastUpdate);
+        cbMenu.update(milliSinceLastUpdate);
+    }
+
+    public Camera getCamera(){
+        return camera;
+    }
+}
