68ffa2cd9cb4dde5ca1c4c0d6ee0f4fb8efae9f6
Lingondricka <ras.lindgren@telia.com>
2017-05-10 14:53:13 +0200

    Made basic cardboard button models

---
+++ core/src/com/tda367/parallax/model/cardboardmenu/CardboardStartButton.java
@@ -0,0 +1,114 @@
+package com.tda367.parallax.model.cardboardmenu;
+
+import com.tda367.parallax.model.coreabstraction.Model;
+import com.tda367.parallax.model.coreabstraction.RenderManager;
+import com.tda367.parallax.model.coreabstraction.Renderable;
+import com.tda367.parallax.model.parallaxcore.collision.Collidable;
+import com.tda367.parallax.model.parallaxcore.collision.CollidableType;
+import com.tda367.parallax.model.parallaxcore.collision.CollisionManager;
+
+import javax.vecmath.Quat4f;
+import javax.vecmath.Vector3f;
+
+/**
+ * Created by Rasmus on 2017-05-08.
+ */
+public class CardboardStartButton implements Renderable,Collidable {
+
+    private Vector3f pos;
+    private Quat4f rot;
+    private boolean isHit;
+
+    private Model model;
+
+    private Model collisionModel;
+
+    private boolean collisionEnabled;
+
+    CardboardStartButton(){
+        pos = new Vector3f();
+        rot = new Quat4f();
+        model = new Model("boxObstacle.g3db", "3dModels/boxObstacle");
+        collisionModel = new Model(model.getModelName(), model.getModelDirectory());
+        collisionEnabled = true;
+    }
+
+
+    @Override
+    public Model getModel() {
+        return model;
+    }
+
+    @Override
+    public void addToRenderManager() {
+        RenderManager.getInstance().addRenderTask(this);
+    }
+
+    @Override
+    public void removeFromRenderManager() {
+        RenderManager.getInstance().removeRenderTask(this);
+    }
+
+    @Override
+    public Vector3f getPos() {
+        return pos;
+    }
+
+    @Override
+    public Quat4f getRot() {
+        return rot;
+    }
+
+
+    public boolean isActive() {
+        return collisionEnabled;
+    }
+
+    @Override
+    public boolean collisionActivated() {
+        return false;
+    }
+
+    @Override
+    public void disableCollision() {
+        collisionEnabled = false;
+    }
+
+    @Override
+    public void enableCollision() {
+        collisionEnabled = true;
+    }
+
+    @Override
+    public Model getCollisionModel() {
+        return collisionModel;
+    }
+
+    @Override
+    public void addToCollisionManager() {
+        CollisionManager.getInstance().addCollisionCheck(this);
+    }
+
+    @Override
+    public void removeFromCollisionManager() {
+        CollisionManager.getInstance().removeCollisionCheck(this);
+    }
+
+    @Override
+    public CollidableType getCollidableType() {
+        return null;
+    }
+
+    @Override
+    public void handleCollision(CollidableType type) {
+        if(type == CollidableType.HARMFUL){
+            isHit = true;
+        }
+    }
+    public void setPos(Vector3f vector3f){
+        pos = vector3f;
+    }
+    public boolean isCollided(){
+        return isHit;
+    }
+}
