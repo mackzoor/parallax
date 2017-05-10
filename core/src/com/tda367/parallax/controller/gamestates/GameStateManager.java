dd8a9c81826a9881d9f0c79d33fe5d3019539d7d
Lingondricka <ras.lindgren@telia.com>
2017-05-10 15:01:51 +0200

    Added some setScreen methods to the GameStateManager

--- core/src/com/tda367/parallax/controller/gamestates/GameStateManager.java
+++ core/src/com/tda367/parallax/controller/gamestates/GameStateManager.java
@@ -1,6 +1,7 @@
 package com.tda367.parallax.controller.gamestates;
 
 import com.badlogic.gdx.Game;
+import com.tda367.parallax.CardboardGame;
 
 public final class GameStateManager {
 
@@ -8,6 +9,8 @@ public final class GameStateManager {
 
     private static MainMenuScreen mainMenuScreen;
     private static GameScreen gameScreen;
+    private static CardboardGameScreen cardboardGameScreen;
+    private static CardboardMenuScreen cardboardMenuScreen;
 
     public static void setMainMenuScreen(Game game) {
         if (mainMenuScreen == null) {
@@ -16,10 +19,24 @@ public final class GameStateManager {
         game.setScreen(mainMenuScreen);
     }
 
-    static void setGameScreen(Game game) {
+    public static void setGameScreen(Game game) {
         if (gameScreen == null) {
             gameScreen = new GameScreen(game);
         }
         game.setScreen(gameScreen);
+    }
+
+    public static void setCardboardGameScreen(CardboardGame game){
+        if(cardboardGameScreen == null){
+            cardboardGameScreen = new CardboardGameScreen(game);
+        }
+        game.setCardboardScreen(cardboardGameScreen);
+    }
+
+    public static void setCardboardMenuScreen(CardboardGame game){
+        if(cardboardMenuScreen == null){
+            cardboardMenuScreen = new CardboardMenuScreen(game);
+        }
+        game.setCardboardScreen(cardboardMenuScreen);
     }
 }
