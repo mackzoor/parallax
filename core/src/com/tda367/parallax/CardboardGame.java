22ec906371232a31e82d15aeb1c6a01d2382070a
Lingondricka <ras.lindgren@telia.com>
2017-05-10 14:59:27 +0200

    Created an abstract cardBoardGame class which will act as the Libgdx game but for cardboard

---
+++ core/src/com/tda367/parallax/CardboardGame.java
@@ -0,0 +1,80 @@
+package com.tda367.parallax;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.android.CardBoardApplicationListener;
+import com.google.vrtoolkit.cardboard.Eye;
+import com.google.vrtoolkit.cardboard.HeadTransform;
+import com.google.vrtoolkit.cardboard.Viewport;
+
+/**
+ * Created by Rasmus on 2017-05-10.
+ */
+public abstract class CardboardGame extends Game implements CardBoardApplicationListener {
+
+    protected com.tda367.parallax.controller.gamestates.CardboardScreen cardboardScreen;
+
+    @Override
+    public void onNewFrame(HeadTransform paramHeadTransform) {
+
+    }
+
+    @Override
+    public void onDrawEye(Eye paramEye) {
+
+    }
+
+    @Override
+    public void onFinishFrame(Viewport paramViewport) {
+
+    }
+
+    @Override
+    public void onRendererShutdown() {
+
+    }
+
+    @Override
+    public void onCardboardTrigger() {
+
+    }
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    public void setCardboardScreen(com.tda367.parallax.controller.gamestates.CardboardScreen screen){
+        if (this.cardboardScreen != null) this.cardboardScreen.hide();
+        this.cardboardScreen = screen;
+        if (this.cardboardScreen != null) {
+            this.cardboardScreen.show();
+            this.cardboardScreen.resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        }
+    }
+}
