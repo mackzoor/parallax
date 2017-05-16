2ea46e380c288a7d19f678287c949a2930d9d2ff
Ludvig "Support" Andersson <Ludvand@student.chalmers.se>
2017-05-16 15:31:40 +0200

    Added a time to score method

--- core/src/com/tda367/parallax/model/parallaxcore/Player.java
+++ core/src/com/tda367/parallax/model/parallaxcore/Player.java
@@ -2,10 +2,11 @@ package com.tda367.parallax.model.parallaxcore;
 
 import com.tda367.parallax.model.parallaxcore.spacecraft.ISpaceCraft;
 import lombok.Getter;
+import lombok.Setter;
 
 public class Player {
 
-    @Getter private int score;
+    @Getter @Setter private int score;
     @Getter private String userName;
     @Getter private ISpaceCraft spaceCraft;
 
