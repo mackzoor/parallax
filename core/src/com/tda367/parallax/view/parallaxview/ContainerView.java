8c6396b4e86b998abf47945e94c3e1888897ccf7
NinjaWazzzabi <amk1997@hotmail.se>
2017-05-23 17:00:55 +0200

    Early implementation of powerup display in a container.

--- core/src/com/tda367/parallax/view/parallaxview/ContainerView.java
+++ core/src/com/tda367/parallax/view/parallaxview/ContainerView.java
@@ -5,14 +5,21 @@ import com.tda367.parallax.view.rendering.Renderer3D;
 import com.tda367.parallax.view.rendering.Renderable3dObject;
 import com.tda367.parallax.utilities.ResourceLoader;
 
+import javax.vecmath.Quat4f;
+
 /**
  * View class for {@link Container}.
  */
 public class ContainerView implements View {
 
-    private final static String MODEL_3D_INTERNAL_PATH = "3dModels/mysterycontainer/mysterycontainer.g3db";
+    private final static String MODEL_3D_INTERNAL_PATH = "3dModels/containerround/containerround.g3db";
+
+    //TODO Change path to the particular powerup.
+    private final static String MODEL_3D_INTERNAL_PATH_MISSILE = "3dModels/missile/missile.g3db";
     private final Container container;
-    private Renderable3dObject renderable3dObject;
+    private Renderable3dObject container3dObject;
+    private Renderable3dObject powerUp3dObject;
+    private Quat4f powerUpRotation;
 
     /**
      * Creates a ContainerView from a {@link Container}.
@@ -20,20 +27,41 @@ public class ContainerView implements View {
      */
     ContainerView(Container container) {
         this.container = container;
-        renderable3dObject = new Renderable3dObject(
+        powerUpRotation = new Quat4f(0,0,0,1);
+        container3dObject = new Renderable3dObject(
                 container.getPos(),
                 container.getRot(),
                 ResourceLoader.getInstance().getModel(MODEL_3D_INTERNAL_PATH),
                 1f
+        );
+
+        powerUp3dObject = new Renderable3dObject(
+                container.getPos(),
+                powerUpRotation,
+                ResourceLoader.getInstance().getModel(MODEL_3D_INTERNAL_PATH_MISSILE),
+                1f,
+                true
         );
     }
 
     @Override
     public void render() {
         if (!container.isCollected()){
-            Renderer3D.getInstance().addObjectToFrame(renderable3dObject);
+            updatePowerUpRotation();
+            Renderer3D.getInstance().addObjectToFrame(powerUp3dObject);
+            Renderer3D.getInstance().addObjectToFrame(container3dObject);
         }
     }
+
+    private void updatePowerUpRotation(){
+        //Set rotation of 2 degrees per update.
+        Quat4f rotation = new Quat4f(0,0,   0.035f, 0.999f);
+        rotation.normalize();
+        powerUpRotation.mul(rotation);
+        powerUpRotation.normalize();
+        powerUp3dObject.setRot(powerUpRotation);
+    }
+
     @Override
     public boolean isObsolete() {
         return !container.isCollected();
