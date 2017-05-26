d83f931121247f3ee66e9000f690cf802f88c6dc
Lingondricka <ras.lindgren@telia.com>
2017-05-26 09:41:20 +0200

    Removed some methods which wont be tested

--- core/test/com/tda367/parallax/model/core/powerups/arsenal/CannonTest.java
+++ core/test/com/tda367/parallax/model/core/powerups/arsenal/CannonTest.java
@@ -4,17 +4,12 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by Rasmus on 2017-05-11.
- */
+
 public class CannonTest {
     @Test
     public void activate() throws Exception {
     }
 
-    @Test
-    public void isActive() throws Exception {
-    }
 
     @Test
     public void use() throws Exception {
@@ -29,55 +24,7 @@ public class CannonTest {
     }
 
     @Test
-    public void getPos() throws Exception {
-    }
-
-    @Test
-    public void getRot() throws Exception {
-    }
-
-    @Test
-    public void collisionActivated() throws Exception {
-    }
-
-    @Test
-    public void disableCollision() throws Exception {
-    }
-
-    @Test
-    public void enableCollision() throws Exception {
-    }
-
-    @Test
-    public void getCollisionModel() throws Exception {
-    }
-
-    @Test
-    public void addToCollisionManager() throws Exception {
-    }
-
-    @Test
-    public void removeFromCollisionManager() throws Exception {
-    }
-
-    @Test
-    public void getCollidableType() throws Exception {
-    }
-
-    @Test
     public void handleCollision() throws Exception {
-    }
-
-    @Test
-    public void addToRenderManager() throws Exception {
-    }
-
-    @Test
-    public void getModel() throws Exception {
-    }
-
-    @Test
-    public void removeFromRenderManager() throws Exception {
     }
 
 }
