f46dfdba4223f1ac12fec4f751a18160be4bf691
Lingondricka <ras.lindgren@telia.com>
2017-05-26 09:39:43 +0200

    Some spacecraft tests

--- core/test/com/tda367/parallax/model/core/spacecraft/SpaceCraftTest.java
+++ core/test/com/tda367/parallax/model/core/spacecraft/SpaceCraftTest.java
@@ -1,75 +1,72 @@
 package com.tda367.parallax.model.core.spacecraft;
 
+import com.tda367.parallax.model.core.powerups.arsenal.Cannon;
+import com.tda367.parallax.model.core.powerups.arsenal.IPowerUp;
+import com.tda367.parallax.model.core.powerups.arsenal.Missile;
+import com.tda367.parallax.model.core.powerups.arsenal.PowerUpFactory;
+import com.tda367.parallax.model.core.powerups.container.Container;
+import com.tda367.parallax.model.core.world.courseobstacles.ObstacleFactory;
 import org.junit.Test;
+
+import javax.vecmath.Vector3f;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by Rasmus on 2017-05-25.
- */
+
 public class SpaceCraftTest {
-    @Test
-    public void setDesiredPanVelocity() throws Exception {
-    }
 
-    @Test
-    public void setMaxPanVelocity() throws Exception {
-    }
 
-    @Test
-    public void setForwardSpeedTarget() throws Exception {
-    }
+    SpaceCraft spaceCraft = SpaceCraftFactory.getAgelionInstance(10);
 
-    @Test
-    public void setForwardAcceleration() throws Exception {
-    }
 
     @Test
     public void update() throws Exception {
-    }
+        spaceCraft.getPos().set(500,0,500);
+        spaceCraft.update(1000);
+        assertTrue(spaceCraft.getPos().z < 3 && spaceCraft.getPos().x < 3);
 
-    @Test
-    public void setDesiredPanVelocity1() throws Exception {
-    }
-
-    @Test
-    public void setCurrentPanVelocity() throws Exception {
+        spaceCraft.setForwardAcceleration(10);
+        spaceCraft.update(1000);
+        assertTrue(spaceCraft.getForwardVelocity() == 20);
     }
 
     @Test
     public void action() throws Exception {
+        spaceCraft.add(PowerUpFactory.createCannon());
+        spaceCraft.add(PowerUpFactory.createCannon());
+        int size1 = spaceCraft.getPu().size();
+        IPowerUp pu = spaceCraft.getPu().get(1);
+        spaceCraft.action();
+        assertTrue(pu.isActive());
+        int size2 = spaceCraft.getPu().size();
+        assertTrue(size1 > size2);
+
     }
 
     @Test
     public void add() throws Exception {
-    }
+        spaceCraft.add(PowerUpFactory.createCannon());
+        spaceCraft.add(PowerUpFactory.createMissile());
+        spaceCraft.add(PowerUpFactory.createMissile());
+        spaceCraft.add(PowerUpFactory.createCannon());
+        assertTrue(spaceCraft.getPu().size() == 2);
 
-    @Test
-    public void incHealth() throws Exception {
-    }
-
-    @Test
-    public void decHealth() throws Exception {
-    }
-
-    @Test
-    public void getType() throws Exception {
-    }
-
-    @Test
-    public void collisionActivated() throws Exception {
     }
 
     @Test
     public void handleCollision() throws Exception {
+        int health = spaceCraft.getHealth();
+        spaceCraft.handleCollision(ObstacleFactory.getRandomWallInstance(new Vector3f()));
+        int health2 = spaceCraft.getHealth();
+        assertTrue(health > health2);
+
+        spaceCraft.add(PowerUpFactory.createCannon());
+        int puSize = spaceCraft.getPu().size();
+        spaceCraft.handleCollision(new Container(PowerUpFactory.createCannon()));
+        int puSize2 = spaceCraft.getPu().size();
+        assertTrue(puSize2 > puSize);
+        assertTrue(spaceCraft.getPu().get(1) instanceof Cannon);
     }
 
-    @Test
-    public void setCurrentPanVelocity1() throws Exception {
-    }
-
-    @Test
-    public void setIndependentRotation() throws Exception {
-
-    }
 }
