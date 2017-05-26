0b46208567411b706264832d8748c7770de680d3
NinjaWazzzabi <amk1997@hotmail.se>
2017-05-26 08:17:00 +0200

    Added collision effect class.

---
+++ core/src/com/tda367/parallax/view/parallaxview/CollisionEffect.java
@@ -0,0 +1,81 @@
+package com.tda367.parallax.view.parallaxview;
+
+
+import com.tda367.parallax.model.core.collision.*;
+import com.tda367.parallax.view.rendering.ParticleEffectType;
+import com.tda367.parallax.view.rendering.RenderableParticleEffect;
+import com.tda367.parallax.view.rendering.Renderer3D;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.vecmath.Vector3f;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.tda367.parallax.model.core.collision.CollidableType.CONTAINER;
+import static com.tda367.parallax.model.core.collision.CollidableType.HARMFUL;
+import static com.tda367.parallax.model.core.collision.CollidableType.OBSTACLE;
+
+/**
+ * Listens to collisions and creates explosion effects at the location.
+ */
+public class CollisionEffect implements CollisionObserver{
+
+    private List<RenderableParticleEffect> activeEffects;
+    private CollisionManager collisionManager;
+    @Setter @Getter private boolean enabled;
+
+    CollisionEffect() {
+        activeEffects = new ArrayList<RenderableParticleEffect>();
+        enabled = true;
+        collisionManager = CollisionManager.getInstance();
+        collisionManager.subscribeToCollisions(this);
+    }
+
+    @Override
+    public void respondToCollision(CollisionResult collisionResult) {
+
+        handleExplosion(collisionResult.getFirst());
+        handleExplosion(collisionResult.getSecond());
+
+        Vector3f explosionPoint = collisionResult.getContactPoint();
+        CollidableType first = collisionResult.getFirst().getCollidableType();
+        CollidableType second = collisionResult.getSecond().getCollidableType();
+
+        if (isDestructive(first)  || isDestructive(second)) {
+            createExplosion(explosionPoint);
+        }
+    }
+
+    private boolean isDestructive(CollidableType type) {
+        return type == OBSTACLE || type == HARMFUL;
+    }
+
+    private void handleExplosion(Collidable collidable){
+        if (collidable.getCollidableType() == CONTAINER) {
+            createAscend(collidable.getPos());
+        }
+    }
+
+    private void createAscend(Vector3f position) {
+        RenderableParticleEffect ascendEffect = new RenderableParticleEffect(ParticleEffectType.ASCEND);
+        ascendEffect.setPosition(position);
+        activeEffects.add(ascendEffect);
+        ascendEffect.start();
+    }
+
+    private void createExplosion(Vector3f explosionPoint) {
+        RenderableParticleEffect explosionParticle = new RenderableParticleEffect(ParticleEffectType.EXPLOSION);
+        explosionParticle.setPosition(explosionPoint);
+        activeEffects.add(explosionParticle);
+        explosionParticle.start();
+    }
+
+    public void render(){
+        Renderer3D renderer = Renderer3D.getInstance();
+        for (RenderableParticleEffect particleExplosion : activeEffects) {
+            renderer.addParticleEffectToFrame(particleExplosion);
+        }
+    }
+
+}
