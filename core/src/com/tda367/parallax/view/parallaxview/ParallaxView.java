69d7db41c9d13f733b9dee0214a1ce97db6274a6
NinjaWazzzabi <amk1997@hotmail.se>
2017-05-26 08:18:09 +0200

    Implemented collision effect class in parallax view.

--- core/src/com/tda367/parallax/view/parallaxview/ParallaxView.java
+++ core/src/com/tda367/parallax/view/parallaxview/ParallaxView.java
@@ -15,6 +15,7 @@ public class ParallaxView implements View{
     private final Parallax parallax;
     private WorldView worldView;
     private HudView playerHudView;
+    private CollisionEffect collisionEffect;
     @Getter @Setter private boolean hudViewActive;
 
     /**
@@ -31,24 +32,10 @@ public class ParallaxView implements View{
         );
 
         this.parallax = parallax;
-        playerHudView = new HudView(parallax.getPlayer());
-        worldView = new WorldView(parallax.getWorld());
+        this.playerHudView = new HudView(parallax.getPlayer());
+        this.worldView = new WorldView(parallax.getWorld());
         this.hudViewActive = false;
-    }
-
-    /**
-     * Sets render x-resolution.
-     * @param width new x-resolution.
-     */
-    public void setWidth(int width){
-        Renderer3D.getInstance().setWidth(width);
-    }
-    /**
-     * Sets render y-resolution.
-     * @param height new y-resolution.
-     */
-    public void setHeight(int height){
-        Renderer3D.getInstance().setHeight(height);
+        this.collisionEffect = new CollisionEffect();
     }
 
     @Override
@@ -62,10 +49,19 @@ public class ParallaxView implements View{
         if (hudViewActive) {
             playerHudView.render();
         }
+
+        collisionEffect.render();
         Renderer3D.getInstance().renderFrame();
     }
     @Override
     public boolean isObsolete() {
         return false;
+    }
+
+    public void setWidth(int width){
+        Renderer3D.getInstance().setWidth(width);
+    }
+    public void setHeight(int height){
+        Renderer3D.getInstance().setHeight(height);
     }
 }
