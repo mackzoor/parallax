c355f95dda8fa0c54786125d1e3373b49a6d8732
mackzoor <markus.pettersson97@gmail.com>
2017-05-28 11:44:17 +0200

    Cleaned up Missile

--- core/src/com/tda367/parallax/model/core/powerups/arsenal/Missile.java
+++ core/src/com/tda367/parallax/model/core/powerups/arsenal/Missile.java
@@ -13,72 +13,49 @@ import javax.vecmath.Vector3f;
 
 public class Missile extends PowerUpBase {
 
-     /*
-        Variables for toggling the missile movement
-    */
 
-    //Time to represent the falling phase, value compared to the timeStorage variable
     private static final int FALL_TIME = 500;
-
-    //Time to represent the active phase, value compared to the timeStorage variable
     private static final int ACTIVE_TIME = 5000;
-
-    //Time that missile is tracking the ships movement, making sure that it doesn't go trough it.
     private static final int TIME_TRACKING_TRANS = 700;
-
-    //Multiplier to change the speed of missile
+    private static final float ACCELERATION = 0.8f;
+    private static final float MAXIMUM_VELOCITY = 80;
     private static final float VELOCITY_MULTIPLIER_X = 10f;
     private static final float VELOCITY_MULTIPLIER_Y = 1f;
     private static final float VELOCITY_MULTIPLIER_Z = 10f;
     private static final float FALL_MULTIPLIER = 2.5f;
-
     private static final String COLLISION_MODEL = "3dModels/missile/hitbox.obj";
 
-    //How fast the missile will accelerate
-    private static final double ACCELERATION = 0.8;
-
-    //Maximum velocity that the missile can reach.
-    private static final float MAXIMUM_VELOCITY = 80;
-
-
-    private float startVelocity;
-
-    //Velocity that the missile is moving in.
     @Setter
     private float velocity;
+    private float startVelocity;
+
     private int timeAccelerated;
-
-    //Saving variables for the transformable object the missile has its origin from
-    private Vector3f transformableEarlierPosition;
-    private Transformable transformable;
-    //Storing the position of the transformable object since last update, making it possible to track its movement
-    private Vector3f transPosLastUpdate;
-
     private int timeStorage;
 
+    private Transformable transformable;
+    private Vector3f posLastUpdate;
     @Getter
     private final Vector3f enemyTargetPosition;
 
     Missile() {
         super();
         this.enemyTargetPosition = new Vector3f();
-        this.transPosLastUpdate = new Vector3f();
     }
 
-    //IPowerUp
     @Override
     public void activate(Transformable transformable) {
         super.activate(transformable);
         super.addToCollisionManager();
         super.enableCollision();
-        //Add the missile to the world and make it collidable
-
         this.transformable = transformable;
 
 
         this.enemyTargetPosition.set(new Vector3f(super.getPos()));
         this.enemyTargetPosition.add(new Vector3f(0, 100, 20));
         playMissileSound();
+
+        this.posLastUpdate = new Vector3f(transformable.getPos());
+        super.getPos().set(this.transformable.getPos());
     }
 
     @Override
@@ -110,7 +87,6 @@ public class Missile extends PowerUpBase {
         }
     }
 
-
     //Usable
     @Override
     public void use() {
@@ -119,69 +95,36 @@ public class Missile extends PowerUpBase {
 
     //TODO, remove the fact that missile moves faster towards things further away.
 
-    //Updatable, things that control the missile each update
     @Override
     public void update(int milliSinceLastUpdate) {
         if (!super.isDead() && super.isActive()) {
             this.timeStorage = this.timeStorage + milliSinceLastUpdate;
-
-            //Checks to see that the missile has gotten a velocity before moving
-            //Before testing velocity, check that velocity generation is allowed,
-            //by using previous and current position.
-            if (this.transformableEarlierPosition == null) {
-                this.transformableEarlierPosition = new Vector3f(this.transformable.getPos());
-                //Make sure that the missile has the same position as the ship after one cycle.
-                super.getPos().set(this.transformable.getPos());
-            } else if (this.velocity == 0) {
-                //Generate velocity for the missile, based on the velocity of the transformable object
-                generateVelocity(this.transformableEarlierPosition,
-                        this.transformable,
+            if (this.velocity == 0) {
+                generateVelocity(this.posLastUpdate,
+                        this.transformable.getPos(),
                         milliSinceLastUpdate);
             } else {
-                moveTheMissile(milliSinceLastUpdate,
-                        this.timeStorage);
+                moveTheMissile(milliSinceLastUpdate, this.timeStorage);
             }
+            this.posLastUpdate = new Vector3f(this.transformable.getPos());
         }
     }
 
-
-    //Methods for movement
-
-    private void generateVelocity(Vector3f transformableEarlierPostion, Transformable transformable, int milliSinceLastUpdate) {
-        setVelocity((transformable.getPos().getY() - transformableEarlierPostion.getY()) / ((float) milliSinceLastUpdate) * 1000);
+    private void generateVelocity(Vector3f firstPosition, Vector3f secondPosition, int milliSinceLastUpdate) {
+        setVelocity((secondPosition.getY() - firstPosition.getY()) / ((float) milliSinceLastUpdate) * 1000);
     }
 
     private void moveTheMissile(int milliSinceLastUpdate, int timeStorage) {
-        //Method for general missile movement, separated from update for nicer looking code.
-
-        if (timeStorage <= TIME_TRACKING_TRANS) {
-            if (this.transPosLastUpdate.getX() == 0
-                    && this.transPosLastUpdate.getY() == 0
-                    && this.transPosLastUpdate.getZ() == 0) {
-
-                super.setPos(new Vector3f(this.transformable.getPos()));
-                this.transPosLastUpdate = new Vector3f(this.transformable.getPos());
-            } else {
-                if (this.transPosLastUpdate.getZ() > this.transformable.getPos().getZ()) {
-                    super.getPos().add(new Vector3f(0,
-                            0,
-                            this.transformable.getPos().getZ() - this.transPosLastUpdate.getZ()));
-                }
-            }
-            this.transPosLastUpdate = new Vector3f(this.transformable.getPos());
+        if (timeStorage <= TIME_TRACKING_TRANS &&
+                this.posLastUpdate.getZ() > this.transformable.getPos().getZ()) {
+            super.getPos().add(new Vector3f(0, 0,
+                    this.transformable.getPos().getZ() - this.posLastUpdate.getZ())
+            );
         }
-
-        //Check if the missile is in the falling phase, moving to target phase or if the missile should be removed.
         if (timeStorage <= FALL_TIME) {
-            moveOnVelocity(milliSinceLastUpdate);
-            fall(milliSinceLastUpdate);
-
-
+            this.duringFallTime(milliSinceLastUpdate);
         } else if (timeStorage <= ACTIVE_TIME) {
-            super.enableCollision();
-            accelerateMissile(milliSinceLastUpdate);
-            moveOnVelocity(milliSinceLastUpdate);
-
+            this.duringActiveTime(milliSinceLastUpdate);
             if (timeStorage > TIME_TRACKING_TRANS) {
                 final Vector3f directionalVector = generateDirectionVector(getEnemyTargetPosition());
                 moveOnDirectionVector(directionalVector, milliSinceLastUpdate);
@@ -190,6 +133,17 @@ public class Missile extends PowerUpBase {
         } else {
             removeMissile();
         }
+    }
+
+    private void duringFallTime(int milliSinceLastUpdate) {
+        moveOnVelocity(milliSinceLastUpdate);
+        fall(milliSinceLastUpdate);
+    }
+
+    private void duringActiveTime(int milliSinceLastUpdate) {
+        super.enableCollision();
+        accelerateMissile(milliSinceLastUpdate);
+        moveOnVelocity(milliSinceLastUpdate);
     }
 
     private void accelerateMissile(int milliSinceLastUpdate) {
@@ -201,12 +155,6 @@ public class Missile extends PowerUpBase {
         }
 
         if (this.velocity < MAXIMUM_VELOCITY) {
-            /*Calculate the velocity based on this formula: x(t) = x0 × (1 + r)^t
-            x(t) is the value at time t.
-            x0 is the initial value at time t=0.
-            r is the growth rate when r>0 or decay rate when r<0, in percent.
-            t is the time in discrete intervals and selected time units.
-             */
             this.velocity = this.startVelocity * ((float) Math.pow(1 + ACCELERATION, ((double) this.timeAccelerated) / 1000));
         } else {
             this.velocity = MAXIMUM_VELOCITY;
@@ -225,33 +173,25 @@ public class Missile extends PowerUpBase {
     }
 
     private void moveOnVelocity(int milliSinceLastUpdate) {
-        //Method for moving the missile according to the velocity
-
         final float posYAdded = this.velocity * ((float) milliSinceLastUpdate / 1000);
         super.getPos().add(new Vector3f(0, posYAdded, 0));
     }
 
-    //Method to generate a direction vector (normalized) for the ship's movement.
     private Vector3f generateDirectionVector(Vector3f target) {
         final Vector3f directionalVector = new Vector3f(target.getX() - getPos().getX(),
                 target.getY() - getPos().getY(),
                 target.getZ() - getPos().getZ());
 
         directionalVector.normalize();
-
         return directionalVector;
     }
 
-    //Method for moving the ship based on the direction vector generated from method "generateDirectionalVector"
     private void moveOnDirectionVector(Vector3f directionalVector, int milliSinceLastUpdate) {
         getPos().add(new Vector3f((directionalVector.getX() * (float) milliSinceLastUpdate / 1000) * VELOCITY_MULTIPLIER_X,
                 (directionalVector.getY() * (float) milliSinceLastUpdate / 1000) * VELOCITY_MULTIPLIER_Y,
                 (directionalVector.getZ() * (float) milliSinceLastUpdate / 1000) * VELOCITY_MULTIPLIER_Z));
         this.setRot(MathUtilities.vectorToQuat(directionalVector));
     }
-
-
-    //Sound
 
     private void playMissileSound() {
         AudioQueue.getInstance().playSound("MissileDemo.mp3", "sounds/effects", 0.7f);
