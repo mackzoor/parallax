40160dca7110f83b3ca858432f4bc4ea42259695
mackzoor <markus.pettersson97@gmail.com>
2019-11-07 22:13:20 -0600

    Began JSON implementation for StateOutput

--- core/src/com/tda367/parallax/controller/screens/StateOutput.java
+++ core/src/com/tda367/parallax/controller/screens/StateOutput.java
@@ -5,6 +5,8 @@ import com.tda367.parallax.model.core.world.ICourseModule;
 import com.tda367.parallax.model.core.world.World;
 import com.tda367.parallax.model.core.world.courseobstacles.CourseObstacleBase;
 
+import org.json.JSONObject;
+
 import java.net.*;
 import java.io.*;
 
@@ -14,17 +16,28 @@ public class StateOutput {
 
     private static final int PORT = 5000;
     private PrintWriter pr;
+    private OutputStreamWriter out;
 
     public StateOutput() throws IOException{
         ServerSocket ss = new ServerSocket(PORT);
         Socket s = ss.accept();
         System.out.println("Client connected");
-
         this.pr = new PrintWriter(s.getOutputStream());
+        this.out = new OutputStreamWriter(s.getOutputStream(), "UTF-8");
     }
 
     public void update(Parallax parallax) {
-        this.pr.println("UPDATE");
+        JSONObject jo = new JSONObject();
+        // putting data to JSONObject
+        jo.put("firstName", "John");
+        jo.put("lastName", "Smith");
+        jo.put("age", 25);
+        try{
+            this.out.write(jo.toString());
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        /*this.pr.println("UPDATE");
         this.pr.flush();
         this.pr.println("PLAYER" + parallax.getPlayer().getSpaceCraft().getPos().toString());
         this.pr.flush();
@@ -36,7 +49,7 @@ public class StateOutput {
                 this.pr.println(obstacle.getObstacleType() + obstacle.getPos().toString() + obstacle.getRot().toString());
                 this.pr.flush();
             }
-        }
+        }*/
     }
 
 
